b5a0033e3d709410cb24d0e0db978603730de2cc
Mason Kim <sungjinkimm@gmail.com>
2021-11-30 16:34:03 -0500

    minor css adjustment

--- server/schema.sql
+++ server/schema.sql
@@ -1,18 +1,18 @@
-CREATE DATABASE IF NOT EXISTS snippet-app;
+CREATE DATABASE IF NOT EXISTS snippet_app;
 
-USE snippet-app;
+USE snippet_app;
 
-DROP TABLE if EXISTS snippets
+DROP TABLE if EXISTS snippets;
 
 CREATE TABLE snippets (
   id INT AUTO_INCREMENT,
   title VARCHAR(100),
   language VARCHAR (100),
   description VARCHAR (100),
-  snippet VARCHAR (100),
+  snippet LONGTEXT,
   PRIMARY KEY(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 INSERT INTO snippets (title, language, description, snippet)
-VALUES ('Test 1', 'HTML5', 'description goes here', 'snipper goes here');
+VALUES ('Test 1', 'HTML5', 'description goes here', 'snippet goes here');
