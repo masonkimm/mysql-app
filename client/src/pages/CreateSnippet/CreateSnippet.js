b534fc9ba5eaf9ae5bdcd03e836dcb94d1430e41
SungJin Kim <sungjinkimm@gmail.com>
2021-11-30 21:32:24 -0500

    66

--- client/src/pages/CreateSnippet/CreateSnippet.js
+++ client/src/pages/CreateSnippet/CreateSnippet.js
@@ -20,7 +20,9 @@ const CreateSnippet = () => {
     try {
       const res = await axios.post('/snippet/create', newSnippetData);
       const id = res.data.insertId;
-      window.location.replace('/');
+      window.location.href = `/snippet/${id}`;
+
+      // window.location.replace('/');
       // window.location.replace(`/snippet/${id}`);
     } catch (err) {
       console.log(err);
